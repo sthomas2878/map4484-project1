a98ce06448b22036f73ed5d4255870baac3ee050
sthomas2878 <58488914+sthomas2878@users.noreply.github.com>
2023-02-06 14:26:52 -0500

    Update project1R.R

--- project1R.R
+++ project1R.R
@@ -1,16 +1,16 @@
 library(ggplot2)
 library(tidyverse)
+library(ggthemes)
 
 dat <- tibble(Hour = seq(1:168),
               Concentration_High = rep(15,168),
-              Hour_Since_Dose = rep(c(0,1,2,3),42))
-
-dat <- dat %>% mutate(Concentration = 
-                        dat$Concentration_High * (0.5^(dat$Hour_Since_Dose / 4)))
+              Hour_Since_Dose = rep(c(0,1,2,3,0,1,2,3,0,1,2,3,0,1,2,3,0,1,2,3,4,5,6,7),7))
 
 doses <- 42
 dose_strength <- 5
 half_life <- 4
+
+
 concentration <- function(doses, dose_strength, half_life) {
 final_mat <- data.frame(matrix(0, nrow = 168, ncol = doses + 1))
 final_mat[,1] <- rep(0:(168/doses - 1), doses)
@@ -44,28 +44,74 @@ head(final_mat)
 
 final_mat$Total <- apply(final_mat[,-1], MARGIN = 1, sum)
 return(final_mat$Total)
+#return(final_mat)
 }
 
+
+    ######## OPIOIDS ########
 strength_5 <- concentration(doses = 42,
                             dose_strength = 5,
                             half_life = 4)
+strength_10 <- concentration(doses = 28,
+                             dose_strength = 10,
+                             half_life = 4)
 strength_15 <- concentration(doses = 28,
                             dose_strength = 15,
                             half_life = 4)
 
 
 dat$Total_5 <- strength_5
+dat$Total_10 <- strength_10
 dat$Total_15 <- strength_15
 
-adult <- ggplot(dat, aes(x = Hour)) +
+colors = c('15mg every 6 hours' = 'red',
+           '10mg every 6 hours' = 'dark green',
+           '5mg every 4 hours' = 'blue')
+
+adult <- ggplot(dat, aes(x = Hour,color = '5mg every 4 hours')) +
   geom_line(aes(y = Total_5)) + 
   geom_vline(xintercept = c(0, 24, 48, 72, 96, 120, 144, 168),
-             color = 'red', linetype = 2) + 
-  theme_bw() +
-  xlim(0, 24) + # Single Day
-  labs(title = 'Concentration Over Time', y = 'Concentration')
+             linetype = 2) + 
+  theme_solarized() +
+  xlim(0, 48) + # Single Day
+  ylim(0, 16) +
+  labs(title = 'Quantity of Drug in Body Over Time',
+       y = 'Quantity (mg)',
+       color = 'Legend')
 
+adult <- adult + geom_line(aes(y = Total_15, color = '15mg every 6 hours')) +
+  scale_color_manual(values = colors)
+
+adult <- adult + geom_line(aes(y = strength_10, color = '10mg every 6 hours'))
 adult
 
-adult + geom_line(aes(y = Total_15), 
-                  color = 'blue')
+      ######## IBUPROFEN ########
+strength_4hrs <- concentration(doses = 42,
+                            dose_strength = 400,
+                            half_life = 2)
+strength_6hrs <- concentration(doses = 28,
+                             dose_strength = 400,
+                             half_life = 2)
+
+
+dat$Total_4hrs <- strength_4hrs
+dat$Total_6hrs <- strength_6hrs
+
+colors = c('400mg every 4 hours' = 'red',
+           '400mg every 6 hours' = 'dark green')
+
+adult <- ggplot(dat, aes(x = Hour, color = '400mg every 4 hours')) +
+  geom_line(aes(y = Total_4hrs)) + 
+  geom_vline(xintercept = c(0, 24, 48, 72, 96, 120, 144, 168),
+             linetype = 2) + 
+  theme_solarized() +
+  xlim(0, 48) + # Number of hours
+  labs(title = 'Quantity of Drug in Body Over Time',
+       y = 'Quantity (mg)',
+       color = 'Legend')
+adult
+
+adult <- adult + geom_line(aes(y = Total_6hrs,
+                               color = '400mg every 6 hours')) +
+  scale_color_manual(values = colors)
+adult
